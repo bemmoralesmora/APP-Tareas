753d79abe9dcd0894bb0bdfa40b5db1ba42e391f
bemmoralesmora <159475376+bemmoralesmora@users.noreply.github.com>
2025-02-21 10:13:07 -0600

    refactorización

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { header } from "./components/header/header.js";
+import { Cargarheader } from "./components/header/header.js";
 import { formulario } from "./components/formulario/formulario.js";
 import { item } from "./components/item/item.js";
 
 let Dom = document.querySelector("#root");
 Dom.className = "dom";
 
-Dom.appendChild(header());
+Dom.appendChild(Cargarheader());
 Dom.appendChild(item());
 Dom.appendChild(formulario());
+
+function cargaDom (){
+
+}
